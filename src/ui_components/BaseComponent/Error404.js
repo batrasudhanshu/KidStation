62f929258208aeb4cebbc350754fdf508957c0e3
Rakshit Maini <rakshitmaini@gmail.com>
2020-08-28 19:05:25 +0530

    Error404 page updated slighlty

--- src/ui_components/BaseComponent/Error404.js
+++ src/ui_components/BaseComponent/Error404.js
@@ -4,7 +4,12 @@ const Error404 = () => {
   return (
     <>
       <Container maxWidth="sm">
-        <div>You searched for </div>
+        <div
+          style={{ paddingTop: "10rem", height: "50vh", textAlign: "center" }}
+        >
+          <h1>404</h1>
+          <h3>Page Not Found</h3>
+        </div>
       </Container>
     </>
   );
