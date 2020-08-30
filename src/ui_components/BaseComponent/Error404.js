567873be488829ca001f597eb55f0d8df88bce3f
batrasudhanshu <batrasudhanshu09@gmail.com>
2020-08-30 13:48:17 +0530

    Error 404-->UI updated

--- src/ui_components/BaseComponent/Error404.js
+++ src/ui_components/BaseComponent/Error404.js
@@ -1,14 +1,81 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
+import Really from "../../images/really.gif";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Typography from "@material-ui/core/Typography";
+import { Link } from 'react-router-dom'
+let url = window.location.pathname.split("/");
+
+let searchProduct = url[2];
+const searchCollection = url[1];
+console.log(searchCollection, searchProduct);
 const Error404 = () => {
+
+  const popularSearch = [
+    {
+      label: "Erasers",
+      link: "/erasers",
+    },
+    {
+      label: "Lunch Boxes",
+      link: "/lunch_boxes",
+    },
+    {
+      label: "Water Bottles",
+      link: "/water_bottles",
+    },
+    {
+      label: "Pens",
+      link: "/pens",
+    },
+    {
+      label: "Geometry Box",
+      link: "/geometry_boxes",
+    },
+    {
+      label: "Bags",
+      link: "/bags",
+    },
+    {
+      label: "Best Selling",
+      link: "/bestselling",
+    },
+  ];
   return (
     <>
-      <Container maxWidth="sm">
-        <div
-          style={{ paddingTop: "10rem", height: "50vh", textAlign: "center" }}
-        >
-          <h1>404</h1>
-          <h3>Page Not Found</h3>
+      <Container maxWidth="md">
+        <div style={{ paddingTop: "3rem", height: "60vh" }}>
+          <h4 style={{ textAlign: "center" }}>
+            You Searched for{" "}
+            <Link
+              style={{ color: "saddlebrown" }}
+              to={`/${searchCollection}`}
+
+            >
+              {searchCollection}
+            </Link>{" "}
+            / {searchProduct}{" "}
+          </h4>
+          <div style={{ textAlign: "center" }}>
+            <img src={Really} />
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <span style={{ fontSize: "2rem" }}>
+              Sorry, we Couldn't find any matches, checkout some popular searches{" "}
+            </span>
+          </div>
+          <div className="popular-links">
+
+            <Breadcrumbs aria-label="breadcrumb" separator="|" >
+              {popularSearch.map((p) => (
+                <Link to={p.link} style={{ color: "saddlebrown", fontSize: "1.3rem" }}>
+                  {p.label}
+                </Link>
+              ))}
+
+
+            </Breadcrumbs>
+          </div>
         </div>
       </Container>
     </>
