e2404982327af87ba893a78d8d2fb00eb4f08445
Rakshit Maini <rakshit.maini@bivocalbirds.com>
2020-10-06 13:33:29 +0530

    track order page css resolved

--- src/ui_components/TrackOrder/index.js
+++ src/ui_components/TrackOrder/index.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
 
 class TrackOrder extends Component {
-
-
-    render() {
-        return (
-            <>
-                trackorder main page
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 style={{ margin: "20rem 0", textAlign: "center" }}>
+          Coming Soon..
+        </h1>
+      </>
+    );
+  }
 }
 
-export default TrackOrder
+export default TrackOrder;
